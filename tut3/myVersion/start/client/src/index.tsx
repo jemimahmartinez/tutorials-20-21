f4e1c319cf8825eecf90b52b2ed2527e224d97a4
jemimahmartinez <martinezjemimah@gmail.com>
2021-02-09 16:38:56 +1300

    define the query

--- tut3/myVersion/start/client/src/index.tsx
+++ tut3/myVersion/start/client/src/index.tsx
@@ -1,25 +1,28 @@
-import { ApolloClient, gql, NormalizedCacheObject } from '@apollo/client';
+import {
+  ApolloClient,
+  NormalizedCacheObject,
+  ApolloProvider,
+} from '@apollo/client';
 import { cache } from './cache';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pages from './pages';
+import injectStyles from './styles';
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
   uri: 'http://localhost:4000/graphql',
 });
 
-client
-  .query({
-    query: gql`
-      query TestQuery {
-        launch(id: 56) {
-          id
-          mission {
-            name
-          }
-        }
-      }
-    `,
-  })
-  .then((result) => console.log(result));
+injectStyles();
+
+// Pass the ApolloClient instance to the ApolloProvider component
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
 
 // NOTES:
 
@@ -27,7 +30,25 @@ client
 // The `uri` of our GraphQL server (in this case `localhost:4000/graphql`)
 // An instance of `InMemoryCache` to use as the client's `cache` - we import this instance from the `cache.ts` file
 
+// client
+//   .query({
+//     query: gql`
+//       query TestQuery {
+//         launch(id: 56) {
+//           id
+//           mission {
+//             name
+//           }
+//         }
+//       }
+//     `,
+//   })
+//   .then((result) => console.log(result));
+
 // When the index page opens, on the console in the browser - will see a logged `Object` that contains your server's response to your query
 // The data you requested is contained in the object's `data` field, and the other fields provide metadata about the state of the request
 // Can also open the Network tab in browser's developer tools and refresh the page to view the shape of the request that Apollo Client makes to execute your query
 // (its a POST request to `localhost:4000`)
+
+// `ApolloProvider` component is similar to React's context provider: it wraps your React app and places `client` on the context,
+// which enables you to acces it from anywhere in your component tree
--- tut3/myVersion/start/client/src/pages/launches.tsx
+++ tut3/myVersion/start/client/src/pages/launches.tsx
@@ -1,6 +1,6 @@
-import React, { Fragment, useState }  from 'react';
+import React, { Fragment, useState } from 'react';
 import { RouteComponentProps } from '@reach/router';
-import { gql } from '@apollo/client'
+import { gql, useQuery } from '@apollo/client';
 
 export const LAUNCH_TILE_DATA = gql`
   fragment LaunchTile on Launch {
@@ -18,10 +18,35 @@ export const LAUNCH_TILE_DATA = gql`
   }
 `;
 
+export const GET_LAUNCHES = gql`
+  query GetLaunchList($after: string) {
+    launches(after: $after) {
+      cursor
+      hasMore
+      launches {
+        ...LaunchTile
+      }
+    }
+  }
+  ${LAUNCH_TILE_DATA}
+`;
+
 interface LaunchesProps extends RouteComponentProps {}
 
 const Launches: React.FC<LaunchesProps> = () => {
   return <div />;
-}
+};
 
 export default Launches;
+
+// NOTES:
+
+// LAUNCH_TILE_DATA defines a GraphQL fragment, which is named `LaunchTile`
+// A fragment is useful for defining a set of fields that you can include across multiple queries without rewriting them
+// In the query above, we include the `LaunchTile` fragment in our query by preceeding it with `...`,
+// similar to JS spread syntax
+
+// Notice that in addition to fetching a list of `launches`, our query fetches `hasMore` and `cursor` fields.
+// That's because the `launches` query returns paginated results
+// The `hasMore` field indicates whether there are additional launches beyond the list returned by the server
+// The `cursor` field indicates the client's current position within th list of launches. We can execute the query again and provide our most recent `cursor` as the value of the `$after` variable to fetch the next set of launches in the list
